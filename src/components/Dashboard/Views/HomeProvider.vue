9c37c13e5aa985bbb566a7c34ea5073e0991961d
antoncom <anton.com@mail.ru>
2018-08-01 18:24:18 +0300

    Cross-browser support info added

--- src/components/Dashboard/Views/HomeProvider.vue
+++ src/components/Dashboard/Views/HomeProvider.vue
@@ -30,16 +30,23 @@
     <div class="col-md-6 card">
         <div class="card-header">
           <h3 class="title">Заметки</h3>
-          <div class="category">Заметки по обновлениям прототипа.</div>
+          <div class="category">Кросс-броузерная поддержка</div>
         </div>
         <div class="card-content row">
           <div class="col-md-12">
-            <p>Сейчас в работе:</p>
-            <ul>
-              <!--<li>
-                <a href="/#/provider/techproc">Визуализация бизнес-процессов</a>
-              </li>-->
-            </ul>
+            <div class="card-content table-responsive table-full-width">
+              <el-table :data="browsers_compability">
+                <el-table-column label="N" property="id" width="50"></el-table-column>
+                <el-table-column label="Браузер" property="browser" width="160"></el-table-column>
+                <el-table-column label="OS" property="os" width="100"></el-table-column>
+                <el-table-column label="Статус" property="status">
+                  <template slot-scope="scope">
+                    <span v-if="scope.row.status === 'Ok'" class="ti-check" style="color: darkgreen;"></span>
+                    <span v-else style="color: darkred;">{{ scope.row.status }}</span>
+                  </template>
+                </el-table-column>
+              </el-table>
+            </div>
           </div>
         </div>
     </div>
@@ -51,7 +58,45 @@
   export default{
     data () {
       return {
-        links: []
+        links: [],
+        browsers_compability: [
+          {
+            id: 1,
+            browser: 'IE 11.0.9600.1980',
+            os: 'Windows 7',
+            status: 'Ok'
+          },
+          {
+            id: 2,
+            browser: 'Chrome 68.0.3440.84',
+            os: 'Windows 7',
+            status: 'Ok'
+          },
+          {
+            id: 3,
+            browser: 'Firefox 61.0.1',
+            os: 'Windows 7',
+            status: 'Ok'
+          },
+          {
+            id: 4,
+            browser: 'Opera 54.0.2952.64',
+            os: 'Windows 7',
+            status: 'Ok'
+          },
+          {
+            id: 5,
+            browser: 'Яндекс 18.6.1.770',
+            os: 'Windows 7',
+            status: 'Ok'
+          },
+          {
+            id: 6,
+            browser: 'Safari 5.1.7',
+            os: 'Windows 7',
+            status: 'Нет поддержки'
+          }
+        ]
       }
     },
     mounted: function () {
